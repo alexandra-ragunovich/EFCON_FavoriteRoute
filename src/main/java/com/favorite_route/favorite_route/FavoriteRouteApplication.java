d37b3847d404d686c065d2fa89e2a23daf996c80
alexandra <alexandra.ragunovich@gmail.com>
2026-03-23 12:02:46 +0300

    first commit

---
+++ src/main/java/com/favorite_route/favorite_route/FavoriteRouteApplication.java
@@ -0,0 +1,13 @@
+package com.favorite_route.favorite_route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/favorite_route/favorite_route/FavoriteRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favorite_route.favorite_route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
